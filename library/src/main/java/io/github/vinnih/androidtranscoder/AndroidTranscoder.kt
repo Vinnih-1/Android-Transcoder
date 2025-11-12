fb56c1fe56a46d9736dcd960357cea02ccc4da73
Vinícius Albert <looucosad@gmail.com>
2025-11-12 12:13:53 -0300

    feat(converter): extracting to wave format, encode and delete the cached file

--- library/src/main/java/io/github/vinnih/androidtranscoder/AndroidTranscoder.kt
+++ library/src/main/java/io/github/vinnih/androidtranscoder/AndroidTranscoder.kt
@@ -1,21 +1,31 @@
 package io.github.vinnih.androidtranscoder
 
+import android.content.Context
+import android.util.Log
 import io.github.vinnih.androidtranscoder.encoder.EncoderManager
+import io.github.vinnih.androidtranscoder.exceptions.IncompatibleAudioTypeException
+import io.github.vinnih.androidtranscoder.extractor.AudioExtractor
 import io.github.vinnih.androidtranscoder.types.AudioType
 import java.io.File
+
+const val TAG = "AndroidTranscoder"
 
 class AndroidTranscoder(
     val file: File,
     val to: AudioType,
+    val context: Context,
 ) {
-    private val outputFile: File = File(file.parent, file.nameWithoutExtension.plus(to.value))
+    suspend fun converter(): File {
+        if (!EncoderManager.checkCompatibility(file)) {
+            throw IncompatibleAudioTypeException("Incompatible Audio Type")
+        }
+        val reader = AudioExtractor(file, context.cacheDir.absolutePath).extract()
+        val file = EncoderManager.convert(reader, to, context.filesDir.absolutePath)
 
-    suspend fun converter(
-        progress: (Int) -> Unit = {},
-        success: (File) -> Unit = {},
-        error: () -> Unit = {},
-    ) {
-        if (!outputFile.exists()) outputFile.createNewFile()
-        EncoderManager.convert(file, outputFile, to, progress, success, error)
+        reader.data
+            .delete()
+            .also { Log.d(TAG, "Deleting cached wave file") }
+
+        return file
     }
 }
