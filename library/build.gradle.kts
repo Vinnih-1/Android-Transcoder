00fd58152fc497a7289d00d9ac418cdfadeca72f
Vinícius Albert <looucosad@gmail.com>
2025-11-15 10:32:11 -0300

    build(maven): add maven publish plugin

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ktlint.android)
+    alias(libs.plugins.maven.publish)
 }
 
 android {
