2d58148c01d79ec895752f9ad81c7f87e20125e2
Vinícius Albert <looucosad@gmail.com>
2025-11-16 11:25:43 -0300

    init: create an initial example app project

---
+++ app/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.ktlint.android)
+}
+
+android {
+    namespace = "io.github.vinnih.app"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "io.github.vinnih.app"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+ktlint {
+    android.set(true)
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material3)
+    implementation(libs.compose.bom)
+    implementation(libs.preview)
+    implementation(libs.compose.viewmodel)
+
+    testImplementation(libs.junit)
+
+    debugImplementation(libs.preview)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.compose.bom)
+}
---
+++ app/src/main/java/io/github/vinnih/app/MainActivity.kt
@@ -0,0 +1,16 @@
+package io.github.vinnih.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.ComposeView
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ComposeView(this.applicationContext).setContent {
+        }
+    }
+
+    fun Greetings() {
+    }
+}
