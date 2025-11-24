0694cc2e3f92f4601d2a7c6fac9f237da39a84be
Vinícius Albert <looucosad@gmail.com>
2025-11-24 14:37:37 -0300

    feat(app): create an instance of PlayerViewModel and ExoPlayer

--- app/src/main/java/io/github/vinnih/app/MainActivity.kt
+++ app/src/main/java/io/github/vinnih/app/MainActivity.kt
@@ -6,18 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import io.github.vinnih.app.ui.home.HomeScreen
 import io.github.vinnih.app.ui.home.HomeViewModel
+import io.github.vinnih.app.ui.player.PlayerViewModel
 import io.github.vinnih.app.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     private val homeViewModel: HomeViewModel by viewModels()
+    private val playerViewModel: PlayerViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        playerViewModel.init(this.applicationContext)
+
         setContent {
             AppTheme {
                 HomeScreen(
-                    controller = homeViewModel,
+                    homeController = homeViewModel,
+                    playerController = playerViewModel,
                 )
             }
         }
